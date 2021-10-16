b2cc46c46bc1283b6e5c5166ec5b2ccf9d5ff2c7
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-10-16 16:20:48 +0300

    added coin collect and levelUI

--- Scripts/Collectibles/Coin.gd
+++ Scripts/Collectibles/Coin.gd
@@ -7,6 +7,10 @@ func _ready():
 func on_area_entered(area2d):
 	$AnimationPlayer.play("PickUp")
 	call_deferred("disable_pickup")
+	
+	#collected coins script
+	var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
+	baseLevel.coin_collected()
 #prevent the player from picking up the coin again	
 func disable_pickup():
 	$Area2D/CollisionShape2D.disabled = true
