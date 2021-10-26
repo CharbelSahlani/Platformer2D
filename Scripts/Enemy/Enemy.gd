3c1762367b10cfe37259d0cba85a49ad9903bbf4
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-10-26 18:09:43 +0300

    enemy death animation

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+var enemyDeathScene = preload("res://Scenes/EnemyDeath.tscn")
 
 var maxSpeed = 25
 var velocity = Vector2.ZERO
@@ -21,6 +22,16 @@ func _process(delta):
 func on_goal_entered(_aread2d):
 	direction *= -1
 	
+func kill():
+	var deathInstance = enemyDeathScene.instance()
+	get_parent().add_child(deathInstance)
+	deathInstance.global_position = global_position
+	if (velocity.x > 0):
+		deathInstance.scale = Vector2(-1, 1)
+	
+	
+	queue_free()
+
 func on_hitbox_entered(_area2d):
 	$"/root/Helpers".apply_camera_shake(1)
-	queue_free()
+	call_deferred("kill")
