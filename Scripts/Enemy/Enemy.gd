19e88784be1ba4fde0300855778730759f75de56
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-10-28 16:55:24 +0300

    enemy spawn animation

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -1,22 +1,27 @@
 extends KinematicBody2D
 
 var enemyDeathScene = preload("res://Scenes/EnemyDeath.tscn")
+export var isSpawning = true
 
 var maxSpeed = 25
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
 var gravity = 500
 var startDirection = Vector2.RIGHT
+
+
 func _ready():
 	direction = startDirection
 	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 	$HitBoxArea.connect("area_entered", self, "on_hitbox_entered")
 func _process(delta):
+	if (isSpawning):
+		return
 	velocity.x = (direction * maxSpeed).x
 	velocity = move_and_slide((velocity), Vector2.UP)
 	#apply gravity
 	velocity.y += gravity * delta
-	$AnimatedSprite.flip_h = true if direction.x > 0 else false
+	$Visuals/AnimatedSprite.flip_h = true if direction.x > 0 else false
 
 
 func on_goal_entered(_aread2d):
