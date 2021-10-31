ee0c1618e8d0584346b692f753fbbd3a14e69136
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-10-31 11:32:27 +0200

    added button animation scene

---
+++ Scripts/UI/Button.gd
@@ -0,0 +1,19 @@
+extends Button
+
+func _ready():
+	connect("mouse_entered", self, "on_mouse_entered")
+	connect("mouse_exited", self, "on_mouse_exited")
+	connect("pressed", self, "on_pressed")
+	
+	
+func _process(delta):
+	rect_pivot_offset = rect_min_size / 2
+	
+func on_mouse_entered():
+	$HoverAnimationPlayer.play("Hover")
+	
+func on_mouse_exited():
+	$HoverAnimationPlayer.play_backwards("Hover")
+
+func on_pressed():
+	$ClickAnimationPlayer.play("Click")
