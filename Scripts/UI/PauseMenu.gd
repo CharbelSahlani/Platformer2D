d61f41f900fd7c86801d92c2cfc6d277ff0967f8
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-11-01 17:57:11 +0200

    added options menu

--- Scripts/UI/PauseMenu.gd
+++ Scripts/UI/PauseMenu.gd
@@ -4,7 +4,7 @@ onready var continueButton = $MarginContainer/PanelContainer/MarginContainer/VBo
 onready var optionsButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/OptionsButton
 onready var quitToMenuButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton
 
-
+var optionsMenuScene = preload("res://Scenes/UI/OptionsMenu.tscn")
 func _ready():
 	continueButton.connect("pressed", self, "on_continue_pressed")
 	optionsButton.connect("pressed", self, "on_options_pressed")
@@ -24,8 +24,15 @@ func on_continue_pressed():
 	unpause()
 	
 func on_options_pressed():
-	pass
+	var optionsMenuInstance = optionsMenuScene.instance()
+	add_child(optionsMenuInstance)
+	optionsMenuInstance.connect("back_pressed", self, "on_options_back_pressed")
+	$MarginContainer.visible = false
 	
 func on_quit_pressed():
 	$"/root/ScreenTransitionManager".transition_to_menu()
 	unpause()
+
+func on_options_back_pressed():
+	$OptionsMenu.queue_free()
+	$MarginContainer.visible = true
