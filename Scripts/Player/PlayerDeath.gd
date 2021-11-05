337926c92920fa14d66bcfeb4d81a693422e7d19
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-11-05 19:29:59 +0200

    adding sound effects part 1

--- Scripts/Player/PlayerDeath.gd
+++ Scripts/Player/PlayerDeath.gd
@@ -5,7 +5,11 @@ var gravity = 1000
 func _ready():
 	if (velocity.x > 0):
 		$Visuals.scale = Vector2(-1, 1)
-
+	
+	$DeathSoundPlayer1.play()
+	$DeathSoundPlayer2.play()
+	$DeathSoundPlayer3.play()
+	
 func _process(delta):
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
