834ce3adeb6702885190983c24255272528d07f6
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-11-06 10:26:32 +0200

    tweaking UI and sounds

--- Scripts/UI/Button.gd
+++ Scripts/UI/Button.gd
@@ -3,20 +3,28 @@ export(bool) var disableHoverAnim
 func _ready():
 	connect("mouse_entered", self, "on_mouse_entered")
 	connect("mouse_exited", self, "on_mouse_exited")
+	connect("focus_exited", self, "on_focus_exited")
 	connect("pressed", self, "on_pressed")
 	
 	
 func _process(delta):
 	rect_pivot_offset = rect_min_size / 2
 	
+	
+func reset_button_state():
+	if(!disableHoverAnim):
+		$HoverAnimationPlayer.play_backwards("Hover")
+	
 func on_mouse_entered():
 	if(!disableHoverAnim):
 		$HoverAnimationPlayer.play("Hover")
 	
 func on_mouse_exited():
-	if(!disableHoverAnim):
-		$HoverAnimationPlayer.play_backwards("Hover")
+	reset_button_state()
 
 func on_pressed():
 	$AudioStreamPlayer.play()
 	$ClickAnimationPlayer.play("Click")
+
+func on_focus_exited():
+	reset_button_state()
